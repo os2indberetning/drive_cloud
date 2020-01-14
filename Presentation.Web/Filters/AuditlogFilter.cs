9c4b9cdc6cfe0091b0e31efa35f7ebd568d3dee7
Peter Søgaard <pso@digital-identity.dk>
2020-01-14 14:04:21 +0100

    Fixed auditlog filter (it was changing the request before invoking base)

--- Presentation.Web/Filters/AuditlogFilter.cs
+++ Presentation.Web/Filters/AuditlogFilter.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Logging;
-using System;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Identity;
-using Presentation.Web.Auth;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNet.OData.Query;
-using Newtonsoft.Json;
+﻿using Core.DomainModel;
 using Core.DomainServices;
-using Core.DomainModel;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Presentation.Web.Auth;
+using System;
 using System.Security.Claims;
 
 namespace OS2Indberetning.Filters
@@ -52,7 +50,7 @@ namespace OS2Indberetning.Filters
                 logEntry.Location = context.HttpContext.Connection.RemoteIpAddress.ToString();
                 logEntry.Action = ((ControllerActionDescriptor)context.ActionDescriptor).ActionName;
                 logEntry.Controller = ((ControllerActionDescriptor)context.ActionDescriptor).ControllerName;
-                logEntry.Parameters = GetODataParameters(context.ActionArguments);
+                logEntry.Parameters = GetODataParameters(context);
                 logEntry.Timestamp = DateTime.Now;
                 _auditlogRepo.Insert(logEntry);
                 _auditlogRepo.Save();
@@ -65,35 +63,17 @@ namespace OS2Indberetning.Filters
             base.OnActionExecuting(context);
         }
 
-        private string GetODataParameters(IDictionary<string, object> parameters)
+        private string GetODataParameters(ActionExecutingContext context)
         {
-            string result = null;
-            var queryOptionsDictionaryEntry = parameters.Where(x => x.Value is ODataQueryOptions).FirstOrDefault();
             try
             {
-                var queryOptions = queryOptionsDictionaryEntry.Value as ODataQueryOptions;
-                if (queryOptions != null)
-                {
-                    Dictionary<string, string> rawValues = new Dictionary<string, string>();
-                    rawValues.Add("Count", queryOptions.RawValues.Count);
-                    rawValues.Add("Expand", queryOptions.RawValues.Expand);
-                    rawValues.Add("Filter", queryOptions.RawValues.Filter);
-                    rawValues.Add("Format", queryOptions.RawValues.Format);
-                    rawValues.Add("OrderBy", queryOptions.RawValues.OrderBy);
-                    rawValues.Add("Select", queryOptions.RawValues.Select);
-                    rawValues.Add("Skip", queryOptions.RawValues.Skip);
-                    rawValues.Add("SkipToken", queryOptions.RawValues.SkipToken);
-                    rawValues.Add("Top", queryOptions.RawValues.Top);
-
-                    parameters[queryOptionsDictionaryEntry.Key] = rawValues;
-                    result = JsonConvert.SerializeObject(parameters);
-                }
+                return JsonConvert.SerializeObject(((ODataQueryOptions)context.ActionArguments["queryOptions"]).RawValues);
             }
             catch (Exception)
             {
                 // No paramater of type ODataQueryOptions was found in actionContext.ActionArguments, which is fine.
+                return null;
             }
-            return result;
         }
     }
 }
